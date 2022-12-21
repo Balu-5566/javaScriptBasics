5e503bc2f588d0e677f209b6fc6bfc200331b47f
Balu@5566 <balutalupuri5566@gmail.com>
2022-12-21 11:34:48 +0530

    jsbasics

---
+++ Datatypes.js
@@ -0,0 +1,22 @@
+// primitive Data types
+let a = "javascript";
+let e = "name is";
+let flag = true;
+let h=null;
+console.log(typeof(a))
+console.log(typeof(e))
+
+//index string
+console.log(e[7])
+
+console.log(typeof(h))
+console.log(typeof(flag))
+console.log(null);
+
+//symbol
+let sy = Symbol()
+console.log(sy)
+console.log(Symbol()==Symbol());
+
+
+
---
+++ functions.js
@@ -0,0 +1,78 @@
+export  function Add(a,b)
+ {
+    return a+b;
+  //console.log(a+b)
+}
+
+// let sum = add("bala",20)
+// console.log(sum)
+
+
+export  function say()
+{
+    let message="i write functions ";
+    return message;
+   
+}
+
+
+
+
+    
+
+    export function compare(a,b)
+    {
+
+        try
+        {
+
+        if(a>b)
+        {
+            return true;
+        }else if(a<b)
+        {
+            return false;
+        }
+        else
+        {
+            console.log("condition failed");
+        }
+
+} catch (error) 
+{
+    error.console
+}
+
+compare()
+
+}
+// let result = compare(12,10);
+// let result1 = compare(3,9);
+// let reslut2 = compare(12,12);
+// console.log(result);
+// console.log(result1);
+// console.log(reslut2);
+
+
+
+//anonymous function
+// let student = {
+//     name:"Anilkuma",
+//     study:"btec",
+//     address:
+//     {
+//      village:"guntur",
+//      state:"AP"
+//     }
+// }
+// console.log(student.name)
+
+// (function()
+// {
+//     console.log(student.name)
+// })
+// (student);
+
+
+
+
